cc2fd1d241e294a6db2a22fa4ac0761ba382125e
Rita Wolf <nik30003@gmail.com>
2026-08-20 01:26:32 +0300

    made format time by time utils

--- net/ritasister/logging/debug/Debug.cpp
+++ net/ritasister/logging/debug/Debug.cpp
@@ -1,25 +1,20 @@
 #include "Debug.h"
 #include "../../console/Console.h"
+#include "../../util/TimeUtils.h" // Подключаем наш новый класс времени
 
 #ifdef _WIN32
 extern "C" {
     __declspec(dllimport) void* __stdcall CreateFileA(const char*, unsigned long, unsigned long, void*, unsigned long, unsigned long, void*);
     __declspec(dllimport) int __stdcall WriteFile(void*, const void*, unsigned long, unsigned long*, void*);
-    // Структура для системного времени Windows
-    struct SYSTEMTIME {
-        unsigned short wYear, wMonth, wDayOfWeek, wDay, wHour, wMinute, wSecond, wMilliseconds;
-    };
-    __declspec(dllimport) void __stdcall GetLocalTime(SYSTEMTIME* lpSystemTime);
 }
 #elif __linux__
 #include <unistd.h>
 #include <fcntl.h>
-#include <time.h>
 #endif
 
 namespace DebugSystem {
 
-    void writeToFile(const char* text, unsigned long size) {
+    void Logger::writeToFile(const char* text, unsigned long size) {
         const char* filename = "bot.log";
 #ifdef _WIN32
         void* hFile = CreateFileA(filename, 0x00000004, 1, nullptr, 3, 0x00000080, nullptr);
@@ -36,47 +31,14 @@ namespace DebugSystem {
 #endif
     }
 
-    // Вспомогательная функция для добавления ведущего нуля (например, "05" вместо "5")
-    void appendTwoDigits(char* buf, int& index, int value) {
-        buf[index++] = '0' + (value / 10);
-        buf[index++] = '0' + (value % 10);
-    }
-
-    void log(void* hOut, LogLevel level, const char* message) {
+    void Logger::log(void* hOut, LogLevel level, const char* message) {
         if (!message) return;
 
         char logBuffer[512];
         int idx = 0;
 
-        // Получаем реальное системное время
-        int hour = 0, minute = 0, second = 0;
-
-#ifdef _WIN32
-        SYSTEMTIME st;
-        GetLocalTime(&st);
-        hour = st.wHour;
-        minute = st.wMinute;
-        second = st.wSecond;
-#elif __linux__
-        time_t rawtime;
-        time(&rawtime);
-        struct tm* timeinfo = localtime(&rawtime);
-        if (timeinfo) {
-            hour = timeinfo->tm_hour;
-            minute = timeinfo->tm_min;
-            second = timeinfo->tm_sec;
-        }
-#endif
-
-        // 1. Форматируем таймстамп в виде [HH:MM:SS]
-        logBuffer[idx++] = '[';
-        appendTwoDigits(logBuffer, idx, hour);
-        logBuffer[idx++] = ':';
-        appendTwoDigits(logBuffer, idx, minute);
-        logBuffer[idx++] = ':';
-        appendTwoDigits(logBuffer, idx, second);
-        logBuffer[idx++] = ']';
-        logBuffer[idx++] = ' ';
+        // 1. Получаем готовый таймстамп через утилитный класс
+        idx += Utils::TimeUtils::getFormattedTime(logBuffer + idx, sizeof(logBuffer) - idx);
 
         // 2. Уровень логирования
         const char* lvlStr = "[INFO] ";
--- net/ritasister/logging/debug/Debug.h
+++ net/ritasister/logging/debug/Debug.h
@@ -1,6 +1,8 @@
 #pragma once
 
 namespace DebugSystem {
+
+    // Уровни логирования
     enum class LogLevel {
         INFO,
         WARNING,
@@ -8,5 +10,17 @@ namespace DebugSystem {
         DEBUG
     };
 
-    void log(void* hOut, LogLevel level, const char* message);
-}
+    /**
+     * Класс для управления логированием (консоль + файл bot.log)
+     */
+    class Logger {
+    private:
+        static void writeToFile(const char* text, unsigned long size);
+        static void appendTwoDigits(char* buf, int& index, int value);
+
+    public:
+        // Статический метод для логирования (можно вызывать без создания объекта)
+        static void log(void* hOut, LogLevel level, const char* message);
+    };
+
+} // namespace DebugSystem
